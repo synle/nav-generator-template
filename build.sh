2f3d25e6bc0d81a2b0a0f807ef2272ca2fe18762
Sy Le <lenguyensy@gmail.com>
2026-02-09 09:46:48 -0800

    no longer needs to run format

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@ curl -s -S -o index.html https://synle.github.io/nav-generator/index.template.ht
 
 echo 'Download dev.sh template'
 curl -s -S -o dev.sh https://synle.github.io/nav-generator/dev.sh
-
-npm run format
